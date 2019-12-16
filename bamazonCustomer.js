40467ecaeb8b8fd782366b11a204c20e04675029
Juan Rivera <52446143+JuanCodeAtATime@users.noreply.github.com>
2019-12-16 13:30:49 -0500

    Add files via upload

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,26 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+
+// Establishing connection with SQL database
 var connection = mysql.createConnection({
     host: "localhost",
 
     // Your port;
     port: 3306,
 
-    // Your username
+    // My username
     user: "root",
 
-    // Your password
+    // My password
     password: "Daisy2990!",
     database: "bamazon"
 });
 
-//Establishing connection
+//Connecting to the MySQL server and SQL (bamazon) database
+//Console.logging connection ID to confirm connection
+//Invoking userPrompt function at the start of the connection which displays
+//the products available and prompts User with two messages
 connection.connect(function (err) {
 
     if (err) throw err;
@@ -24,7 +29,9 @@ connection.connect(function (err) {
 
 });
 
-
+//userPrompt() features a for loop to display the products table in a neat format
+//The two inquirer.prompt messages also have a validation feature which prevents the User from entering 
+//any key that is not a number 
 function userPrompt() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
@@ -60,10 +67,16 @@ function userPrompt() {
                 }
             }
         ])
-        .then(function (answer) {
 
+        .then(function (answer) {
+            //Connection.query selects the User's ID (answer.choice) from the products table in the SQL bamazon db
             connection.query('SELECT * FROM products WHERE id=' + answer.choice, function (err, results) {
                 if (err) throw err;
+
+                //This conditional statement checks to see if the table's stock quantity is greater than or equal to 
+                //the User's requested item (answer.choice) and quantity (answer.quanity).
+                //If so, the order goes through as well as the transaction $total price.
+
 
                 if (results[0].stock_quantity - answer.quantity >= 0) {
                     console.log("Today's your day!  Bamazon has suffiecient inventory of product ID# " + answer.choice + " to fulfill your order!");
@@ -77,7 +90,7 @@ function userPrompt() {
                         });
 
                 }
-
+                //If not, the User is prompted to re-try the order
                 else {
                     console.log("=======================================\nAww Man!" +
                         "\nInsufficient Stock Alert!  Please re-check the Inventory Qty and re-try your order.\n" +
@@ -88,6 +101,8 @@ function userPrompt() {
             });
         });
 
+    //The nextStep function was created to provide closure (thank you/goodbye greeting) and/or a next step 
+    //based on the User's response
     function nextStep() {
         inquirer
             .prompt({
